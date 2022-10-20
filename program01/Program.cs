05563f3b1c5f8871b7a884047507f416e09e3762
Anderson Nascimento <anderson.nascimento@delage.com.br>
2022-10-19 22:52:07 -0300

    first commit

---
+++ program01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace program01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 900.00, 10);
+
+            p.Nome = "TV 4K";
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
